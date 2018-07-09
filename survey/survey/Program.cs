abcece982f9da51f1c73b75deb89d77ab56da8d9
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-08 22:56:55 -0700

    C# survey page 28

---
+++ survey/survey/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace survey
+{
+    class Program
+    {
+        public static object Response { get; private set; }
+
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            int cnum = Convert.ToInt32(page);
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”");
+            string help = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string pos = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int study = Convert.ToInt32(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
