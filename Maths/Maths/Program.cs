85ac939fc4e0e29a5b7a466e398b7aba8719f343
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-10 13:17:17 -0700

    C# page 32 drill

---
+++ Maths/Maths/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace Maths
+{
+    class Program
+    {
+       
+        static void Main()
+        {
+            //Console.WriteLine("Enter a number");
+            //string number = Console.ReadLine();
+            //int input = Convert.ToInt32(number);
+            //int product = input * 50;
+            //Console.WriteLine(product);
+            //Console.Read();
+
+            //Console.WriteLine("Enter a number");
+            //string number = Console.ReadLine();
+            //int input = Convert.ToInt32(number);
+            //int total = input + 25;
+            //Console.WriteLine(total);
+            //Console.Read();
+
+            //Console.WriteLine("Enter a number");
+            //string number = Console.ReadLine();
+            //double input = Convert.ToDouble(number);
+            //double quotient = input / 12.5;
+            //Console.WriteLine(quotient);
+            //Console.Read();
+
+            //Console.WriteLine("Enter a number");
+            //string number = Console.ReadLine();
+            //int input = Convert.ToInt32(number);
+            //bool TrueOrFalse = input > 50;
+            //Console.Write(TrueOrFalse.ToString());
+            //Console.Read();
+
+            Console.WriteLine("Enter a number");
+            string number = Console.ReadLine();
+            int input = Convert.ToInt32(number);
+            int remainder = input % 7;
+            Console.WriteLine(remainder);
+            Console.Read();
+
+
+
+
+        }
+    }
+}
