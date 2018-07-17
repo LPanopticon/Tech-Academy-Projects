64682f0e66e347b6fdeb8010ecfdbd66aa04772f
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-16 19:26:45 -0700

    C# page 75 Drill

---
+++ Insurance/Insurance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Insurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Insurance Qualification Test");
+            Console.WriteLine("What is your age?");
+            string custage = Console.ReadLine();
+            int Age = Convert.ToInt32(custage);
+
+            Console.WriteLine("True or False, have you ever had a DUI?");
+            string custdui = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(custdui);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string custtickets = Console.ReadLine();
+            int Tickets = Convert.ToInt32(custtickets);
+
+            bool qualified = Age > 15 & DUI == false & Tickets < 3;
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
