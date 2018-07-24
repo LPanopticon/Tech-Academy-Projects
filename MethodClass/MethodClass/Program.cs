47e64a94ccc9f94a348b037f104cebd9506888d1
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-24 02:24:36 -0700

    C# page 110 drill

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            Action actionOne = new Action();
+            actionOne.Number = Convert.ToInt32(Console.ReadLine());
+            actionOne.Add = actionOne.Number + 2;
+            actionOne.Subtract = actionOne.Number - 2;
+            actionOne.Multiply = actionOne.Number * 2;
+            Console.WriteLine(actionOne.Add);
+            Console.WriteLine(actionOne.Subtract);
+            Console.WriteLine(actionOne.Multiply);
+            Console.ReadLine();
+        }
+       
+ 
+          
+    }
+}
