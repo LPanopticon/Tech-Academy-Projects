d7ea6ee54105d9dcf3c51df1037ff5ab817a8474
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-25 16:48:40 -0700

    C# page 112 drill solution

---
+++ MethodClass3/MethodClass/Action.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class Action
+    {       
+        public int Number { get; set; } 
+        public int Add { get; set; }
+        public decimal Subtract { get; set; }
+        public int Multiply { get; set; }
+        
+    }
+
+}
+
+
---
+++ MethodClass3/MethodClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            Action actionOne = new Action
+            {
+                Number = Convert.ToInt32(Console.ReadLine())
+            };
+            actionOne.Add = actionOne.Number + 2;
+            //actionOne.Subtract = actionOne.Number - 2;
+            //actionOne.Multiply = actionOne.Number * 2;
+            Console.WriteLine(actionOne.Add);
+            //Console.WriteLine(actionOne.Subtract);
+            //Console.WriteLine(actionOne.Multiply);
+            Console.ReadLine();
+            Action actionTwo = new Action
+            {
+                Number = (actionOne.Number)
+            };
+            actionTwo.Subtract = actionTwo.Number - 2;
+            Console.WriteLine(actionTwo.Subtract);
+            Console.ReadLine();
+        }   
+       
+ 
+          
+    }
+}
