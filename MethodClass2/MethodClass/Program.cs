000575850719ef40342337d8fcb207d0838c34b7
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-24 18:12:26 -0700

    C# page 112 question

---
+++ MethodClass2/MethodClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass2
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            Action actionOne = new Action
+            {
+                Number = Console.Read()
+            };
+            Console.ReadLine();
+            actionOne.Add = Convert.ToInt32(actionOne.Number + 2);
+            actionOne.Subtract = Convert.ToDecimal(actionOne.Number - 2.5);
+            actionOne.Multiply = Convert.ToInt32(actionOne.Number * 2);
+            Console.WriteLine(actionOne.Add);
+            Console.WriteLine(actionOne.Subtract);
+            Console.WriteLine(actionOne.Multiply);
+            Console.ReadLine();
+        }
+       
+
+ 
+          
+    }
+}
