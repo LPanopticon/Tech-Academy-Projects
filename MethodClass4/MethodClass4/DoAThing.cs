70d8430499ed81d174b8609be7eeff6fd269825b
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-26 00:40:13 -0700

    C# page 113 drill

---
+++ MethodClass4/MethodClass4/DoAThing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass4
+{
+    class DoAThing
+    {
+        public int Math(int _userInput1, int userInput2 = 2)
+        {
+            int result = _userInput1 + userInput2;
+            return result;
+        }
+
+       
+    }
+}
---
+++ MethodClass4/MethodClass4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter another integer");
+            //int input2 = Convert.ToInt32(Console.ReadLine());
+
+            DoAThing doAThing = new DoAThing();
+            int result1 = doAThing.Math(input1);
+            Console.WriteLine(result1);
+            Console.ReadLine();
+            // when the Writeline and input2 variable are commented out, then the 
+            //optional second variable works and adds 2 to the input
+        }
+    }
+}
