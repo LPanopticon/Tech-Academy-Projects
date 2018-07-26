e6421990bf5ed8d188b0648d4e1fb9a75c9132a0
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-26 15:06:24 -0700

    C# page 114 Drill

---
+++ MethodClass5/MethodClass5/DoAnotherThing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass5
+{
+    class DoAnotherThing
+    {
+        public static int Math(int _userInput1 = 2, int _userInput2 = 2, params int[] values)
+        {
+            int sum = 0;
+            foreach (int value in values)
+            {
+                sum += value;
+            }
+            return _userInput2;
+        }
+
+
+
+
+        //(int _userInput1 = 2, int userInput2 = 2)
+        //{
+        //    int result = userInput2;
+        //    return result;
+        //}
+    }
+}
---
+++ MethodClass5/MethodClass5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Enter an integer");
+            //int input1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter another integer");
+            //int input2 = Convert.ToInt32(Console.ReadLine());
+
+            DoAnotherThing doAnotherThing = new DoAnotherThing();
+            DoAnotherThing.Math();
+            Console.WriteLine(DoAnotherThing.Math());
+            Console.ReadLine();
+          
+        }
+    }
+}
+    
+
