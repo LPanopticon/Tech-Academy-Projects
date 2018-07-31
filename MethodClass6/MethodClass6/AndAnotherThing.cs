c06ca979ba0c2bbf9a5f72a70446d7ade7251998
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-31 15:42:12 -0700

    C# Step 116 Drill

---
+++ MethodClass6/MethodClass6/AndAnotherThing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass6
+{
+    class AndAnotherThing
+    {
+        public static void Math(int userInput)
+        {
+            int result = userInput / 2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+      
+        public static void Math(decimal userInput)
+        {
+            decimal result = userInput / 2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodClass6/MethodClass6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass6
+{
+    class Program
+    {
+        static void Main(string[] args)       
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine().ToString();
+
+            if (int.TryParse(input, out int temp1))
+            {
+                AndAnotherThing.Math(temp1);
+            }
+            else if (decimal.TryParse(input, out decimal temp2))
+            {
+                AndAnotherThing.Math(temp2);
+            }
+            else Console.WriteLine("This not an int or decimal");
+            Console.ReadLine();
+
+
+        }
+    }
+}
