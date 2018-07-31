ac1e3b5bc2640bdb72b11911a5c021c064cd0612
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-07-31 12:51:20 -0700

    C# page 114 drill revised

---
+++ MethodClass5Rev/MethodClass5/DoAnotherThing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass5
+{
+    class DoAnotherThing
+    {
+        public static void Math(int _userInput1 = 2, int _userInput2 = 2, params int[] values)
+        {
+            int sum = 0;
+            
+            foreach (int value in values)
+            {
+                sum += value;            
+                Console.WriteLine(sum + " is the sum variable in the loop");
+                Console.WriteLine(value.ToString() + " is a value variable");
+                Console.WriteLine(values + " is in values in the loop"); //doesn't print correctly
+                values.ToList().ForEach(Console.WriteLine); //doesn't print anything (nothing is in values)
+            }
+            int total = sum + _userInput1;
+            values.ToList().ForEach(i => Console.WriteLine(i.ToString())); //doesn't print anything (nothing is in values)
+            Console.WriteLine(values + " doesn't print values (real values or the parameter)");
+            Console.WriteLine(sum + " is the sum variable");
+            Console.WriteLine(_userInput2 + " is _userInput2");
+            Console.WriteLine(total + " is the math operation on the first input required");
+        }
+
+    }
+}
---
+++ MethodClass5Rev/MethodClass5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+     
+
+            DoAnotherThing doAnotherThing = new DoAnotherThing();
+            DoAnotherThing.Math(5, 86);
+            DoAnotherThing.Math(5);
+            Console.WriteLine("------------------------");
+            DoAnotherThing.Math(6, 8, 9, 10, 11);  //Now there will be things in values
+            Console.ReadLine();
+
+        }
+    }
+}
+    
+
