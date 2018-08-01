2b3ed18df92e466ca46ab50b11617cc24d9fa232
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-01 15:40:12 -0700

    C# page 123 drill

---
+++ Abstraction/Abstraction/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Employee : Person
+    {
+        public override void SayName(string FirstName, string LastName)
+        {
+            Person employee = new Employee();           
+            Console.WriteLine("Name: " + FirstName + LastName);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Abstraction/Abstraction/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class Person
+    {
+        
+        public abstract void SayName(string FirstName, string LastName);
+    }  
+       
+       
+}
