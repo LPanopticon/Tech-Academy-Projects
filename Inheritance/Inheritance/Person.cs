663d8cab6596bb81d5d5d16c7c0b7273a0a094df
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-01 13:58:25 -0700

    C# 119 drill

---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        public void SayName(string FirstName, string LastName)
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+            Console.ReadLine();
+        }
+        
+    }
+     
+    
+}
