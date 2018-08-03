02f89b674937377b74c70d9241a87f325f6d4b8a
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-03 00:42:25 -0700

    C# page 125 drill

---
+++ AbstractionInterface/Abstraction/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Employee : Person, IQuittabl
+    {
+        public override void SayName(string FirstName, string LastName)
+        {
+            Person employee = new Employee();           
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.ReadLine();
+
+        }
+
+        public void Quit(string FirstName, string LastName)
+        {
+            Console.WriteLine(FirstName + " " + LastName +" Has Quit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractionInterface/Abstraction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.SayName("Sample", "Student");
+            IQuittabl quittabl = new Employee();
+            employee.Quit("Sample", "Student");
+
+        }
+
+    }
+}
