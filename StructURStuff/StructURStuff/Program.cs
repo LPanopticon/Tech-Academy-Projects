0f25386ec8676feae35a03d3935300fc082b650e
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-08 16:31:45 -0700

    C# page 136

---
+++ StructURStuff/StructURStuff/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructURStuff
+{
+    class Program
+    {
+       public struct Number
+        {
+           public decimal Amount { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 1.5m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
