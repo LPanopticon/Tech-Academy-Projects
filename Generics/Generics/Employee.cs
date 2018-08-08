1c4faeb9055ad5a34c692472241178098d69ef32
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-08 11:38:14 -0700

    C# page 130

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Employee<T> : Person
+    {
+       
+
+        public List<T> Things { get; set; }
+        public static bool operator ==(Employee<T> employee1, Employee<T> employee2)
+
+            {
+                if (employee1.Id == employee2.Id) return true;
+                else return false;
+
+
+
+            }
+            public static bool operator !=(Employee<T> employee1, Employee<T> employee2)
+            {
+                if (employee1.Id != employee2.Id) return true;
+                else return false;
+
+            }
+
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+}
+//public override void SayName(string FirstName, string LastName, int Id)
+//{
+
+
+//}        
+
+//public override int ID(int Id)
+//{          
+//    return Id;
+//}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> empName = new List<string>()
+            {
+                "Logan",
+                "Britadesco"
+            };
+            foreach (string name in empName)
+            {
+                Console.WriteLine(name);
+                Console.ReadLine();
+                    
+            }
+       
+            List<int> empId = new List<int>()
+            {
+                1,
+                2,
+                3,
+                4
+                
+            };
+            foreach (int num in empId)
+            {
+                Console.WriteLine(num);
+                Console.ReadLine();
+            }
+        }
+    }
+}
