92ead9dddf380c04175c7b2ba78f3bb04a80645f
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-08 15:49:39 -0700

    C# page 133 drill

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum1
+{
+    class Program
+    {
+       
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a day of the week");
+
+            try
+            {
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                Console.WriteLine(day);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Check your spelling please");
+                Console.ReadLine();
+            }
+
+
+    }
+    }
+}
+
