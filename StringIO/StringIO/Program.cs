267e73dffa6821694812c5958204db30c7ebfc22
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-11 21:55:06 -0700

    C# page 153 drill

---
+++ StringIO/StringIO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StringIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            string number = string.Format(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\logan\Documents\GitHub\Class Work\StringIO\log.txt", number);
+            string readlog = File.ReadAllText(@"C:\Users\logan\Documents\GitHub\Class Work\StringIO\log.txt");
+            Console.WriteLine(readlog);
+            Console.ReadLine();
+        }
+            
+    }
+}
