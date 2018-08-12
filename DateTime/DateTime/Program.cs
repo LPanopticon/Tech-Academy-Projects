9d52a73153ac0e2cba545785b4d3bb777d25c198
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-11 22:21:48 -0700

    C# page 155 drill

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The time is now " + DateTime.Now);
+            Console.WriteLine("Enter a number");
+            int entTime = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In " + entTime + " Hours the time will be " + (DateTime.Now.AddHours(entTime)));
+            Console.ReadLine();
+          
+        }
+    }
+}
