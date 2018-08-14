2e636e888bfd04b1ac53468c217ab37b4164055e
LPanopticon <39578818+LPanopticon@users.noreply.github.com>
2018-08-13 17:10:21 -0700

    C# page 162 drill

---
+++ ConstVarCon/ConstVarCon/Chain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarCon
+{
+    class Chain
+    {
+        public Chain(string name) : this(name, "Default words")
+        {
+        }
+
+        public Chain(string name, string words)
+        {
+            Name = name;
+            Words = words;
+        }
+        public string Name { get; set; }  
+        public string Words { get; set; }
+    }
+}
---
+++ ConstVarCon/ConstVarCon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarCon
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            var Example = "Here is a string that var knows how to read.";
+            const string exName = "Logan";
+            Console.WriteLine("{0} And a constant just to show I can use it that says my name: {1}", Example, exName);
+
+            //takes the name entered here, and the defaul value of "default words" entered on the class line.
+            Chain chain = new Chain("Logan");
+            Console.WriteLine(chain.Name);
+                   
+            Console.ReadLine();
+        
+
+        }
+    }
+}
